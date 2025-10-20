808e9e5059de7b25e5a39ac34738820fd10b582c
kirylkuchko <kirylkuchko@gmail.com>
2025-10-20 22:49:14 +0200

    Add problem solution of 100 - Same Tree

---
+++ easy/100 - Same Tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+let isSameTree = function(p, q) {
+    if (!p && !q) {
+        return true;
+    }
+    
+    if (p && q && p.val === q.val) {
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+    
+    return false;    
+};
+
+console.log(isSameTree([1,2,3], [1,2,3]));
+console.log(isSameTree([1,2], [1,null,2]));
+console.log(isSameTree([1,2,1], [1,1,2]));
